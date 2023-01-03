2aabfb74a8cda50c8f6f3945d6535810cfde65cc
Joshua Watts <joshwatts592@gmail.com>
2023-01-03 13:50:56 -0330

    Finished project

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour {
+
+    [SerializeField] Text _title,_description,_exhibit;
+    [SerializeField] Image _animalImage;
+    [SerializeField] private CardModel[] _cards;
+
+    
+
+    private void OnEnable()
+    {
+      
+    }
+
+    public void ActivateCard(int pickedCard)
+    {
+        _title.text = _cards[pickedCard].title.ToString();
+        _description.text = _cards[pickedCard].description.ToString();
+        _exhibit.text = _cards[pickedCard].description.ToString();
+        _animalImage.sprite = _cards[pickedCard].animalImage;
+        
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/CardModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards")]
+public class CardModel : ScriptableObject {
+
+   public string title,description,exhibit;
+   public Sprite animalImage;
+   
+}
